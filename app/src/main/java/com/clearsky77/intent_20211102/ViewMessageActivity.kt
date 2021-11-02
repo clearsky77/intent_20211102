56df10b6e1aa35c7be5989750a39d2c7042e53c2
clearsky77 <clearsky3264@gmail.com>
2021-11-02 17:41:48 +0900

    넘어간 화면에서 -> 첨부한 String을 받아서 -> 텍스트뷰에 반영

--- app/src/main/java/com/clearsky77/intent_20211102/ViewMessageActivity.kt
+++ app/src/main/java/com/clearsky77/intent_20211102/ViewMessageActivity.kt
@@ -2,11 +2,13 @@ package com.clearsky77.intent_20211102
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_view_message.*
 
 class ViewMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_message)
-
+        val receivedMessage = intent.getStringExtra("message")
+        txtMessage.text = receivedMessage
     }
 }

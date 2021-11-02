09994fffb8654bf404bcb0c73981c722feb12e40
clearsky77 <clearsky3264@gmail.com>
2021-11-02 17:06:19 +0900

    Intent 활용 - 메인 화면으로 다시 이동

--- app/src/main/java/com/clearsky77/intent_20211102/OtherActivity.kt
+++ app/src/main/java/com/clearsky77/intent_20211102/OtherActivity.kt
@@ -1,11 +1,20 @@
 package com.clearsky77.intent_20211102
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+
+        btnReturnToMain.setOnClickListener {
+            val myIntent = Intent(this, MainActivity::class.java)
+            // 어디서 출발, 어디로 도착
+            startActivity(myIntent)
+        }
     }
 }

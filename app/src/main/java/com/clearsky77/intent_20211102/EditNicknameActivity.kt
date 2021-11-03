41ab60ed39cccd49952c281551c9735d8bcc40ba
clearsky77 <clearsky3264@gmail.com>
2021-11-03 10:18:57 +0900

    닉네임 변경화면 - 확인버튼(이전 화면으로 돌아감)  - 입력된 값 저장  - Intent에 값 첨부  - 결과 설정 (확인 / Intent)  - finish()

--- app/src/main/java/com/clearsky77/intent_20211102/EditNicknameActivity.kt
+++ app/src/main/java/com/clearsky77/intent_20211102/EditNicknameActivity.kt
@@ -1,11 +1,23 @@
 package com.clearsky77.intent_20211102
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nickname.*
 
 class EditNicknameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nickname)
+
+        btnOK.setOnClickListener {
+            val inputNickname = edtNickname.text.toString()
+
+            val resultIntent = Intent() // 왕복이라 출발지, 목적지를 명시할 필요가 없다!
+            resultIntent.putExtra("nick",inputNickname)
+            setResult(RESULT_OK, resultIntent)
+            // 확이 누름, 들고 돌아갈 데이터
+            finish() // 이전 화면으로 돌아간다.
+        }
     }
 }

420972e0238b8eeb8459b2e4174899a2bde1bee8
clearsky77 <clearsky3264@gmail.com>
2021-11-03 10:06:58 +0900

    닉네임 변경 화면 이동  - startActivityForResult 활용  - 멤버변수로 숫자값 (1000) 을 읽기 쉽게 변수에 담아둠

--- app/src/main/java/com/clearsky77/intent_20211102/MainActivity.kt
+++ app/src/main/java/com/clearsky77/intent_20211102/MainActivity.kt
@@ -6,6 +6,9 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val REQ_CODE_FOR_NICKNAME = 1000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,5 +25,15 @@ class MainActivity : AppCompatActivity() {
             myIntent.putExtra("message", inputMessage) // 필요 데이터 첨부
             startActivity(myIntent)
         }
+
+//        닉네임 변경 클릭시
+        btnEditNickname.setOnClickListener {
+            val inputMessage = edtMessage.text.toString()
+            val myIntent = Intent(this, OtherActivity::class.java)
+            myIntent.putExtra("message", inputMessage) // 필요 데이터 첨부
+            startActivityForResult(myIntent, REQ_CODE_FOR_NICKNAME)
+            //                      목적지, 어느 데이터를 받아올 것이다.
+        }
+
     }
 }
